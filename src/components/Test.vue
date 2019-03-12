068d3f86c1c13af07ea546181ba6427762214565
vanya pavlova <vanya.pavlova@primeholding.com>
2019-03-12 14:07:29 +0200

    Vue -demo

---
+++ src/components/Test.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="title"><br />
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.first_name}}</p>
+    <p v-else>Nobody</p>
+    <p v-text="user.first_name"></p>
+    <button v-on:click="greet('Vue click is fired')">Fire click event</button>
+    <br/>
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+    <hr/>
+    <label>First Name: </label>
+    <input type="text" v-model="user.first_name">
+    <br/>
+    <label>Last Name: </label>
+    <input type="text" v-model="user.last_name">
+    <br/>
+    <h3>{{fullName}}</h3>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+     msg: {
+       type: String,
+       default: 'Props Default of Type String'
+     }
+  },
+  data() {
+    return {
+      title: 'Vue Demo',
+      user: {
+        first_name: 'John',
+        last_name: 'Doe'
+      },
+      showName: true,
+      items: [
+        {title: 'Item One'},
+        {title: 'Item Two'},
+        {title: 'Item Three'}
+      ]
+    }
+  },
+  methods: {
+    greet: function(greeting) {
+      alert(greeting);
+    },
+    pressKey: function(e){
+      console.log('Pressed '+ e.target.value);
+    },
+    enterHit: function() {
+      console.log('You hit Enter key');
+    }
+  },
+  computed: {
+    fullName: function(){
+      return this.user.first_name + ' ' + this.user.last_name;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
